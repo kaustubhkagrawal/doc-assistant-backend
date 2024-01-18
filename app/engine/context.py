590125cb9eac00fc4f5f676c0e3598619db63458
Kaustubh Agrawal <kaustubhkagrawal@gmail.com>
2024-01-19 03:42:51 +0530

    remove previous unused code from boilerplate

--- app/engine/context.py
+++ app/engine/context.py
@@ -9,22 +9,13 @@ from llama_index.node_parser import SentenceSplitter
 
 from app.context import create_base_context
 from app.core.config import settings
-from app.engine.constants import CHUNK_SIZE, CHUNK_OVERLAP, NODE_PARSER_CHUNK_OVERLAP, NODE_PARSER_CHUNK_SIZE
+from app.engine.constants import NODE_PARSER_CHUNK_OVERLAP, NODE_PARSER_CHUNK_SIZE
 
 
 
 OPENAI_TOOL_LLM_NAME = "gpt-3.5-turbo-0613"
 OPENAI_CHAT_LLM_NAME = "gpt-3.5-turbo-0613"
 
-
-def create_service_context():
-    base = create_base_context()
-    return ServiceContext.from_defaults(
-        llm=base.llm,
-        embed_model=base.embed_model,
-        chunk_size=CHUNK_SIZE,
-        chunk_overlap=CHUNK_OVERLAP,
-    )
 
 def create_tool_service_context() -> ServiceContext:
     llm = OpenAI(
