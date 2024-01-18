757774147e6f9032a9108d41b54e976d4f7493ff
Kaustubh Agrawal <kaustubhkagrawal@gmail.com>
2024-01-19 02:48:49 +0530

    use OpenAI for embeddings

--- app/context.py
+++ app/context.py
@@ -7,7 +7,6 @@ from llama_index.llms import Ollama, OpenAI
 def create_base_context():
     model = os.getenv("MODEL", "gpt-3.5-turbo")
     return ServiceContext.from_defaults(
-        # llm=OpenAI(model=model),
-        llm = Ollama(model="mistral"), 
-        embed_model='local'
+        llm=OpenAI(model=model),
+        # llm = Ollama(model="mistral"), 
     )
